af85118ffd711b76f7ab45e86713075abb9fde7c
jakee417 <jake.e.taylor1@gmail.com>
2025-01-25 19:22:47 -1000

    remove mobile graph

--- copy.sh
+++ copy.sh
@@ -1,3 +1,2 @@
-cp web_graph.html ~/Documents/jakee417.github.io/assets/html/web_graph.html
-cp mobile_graph.html ~/Documents/jakee417.github.io/assets/html/mobile_graph.html
+cp graph.html ~/Documents/jakee417.github.io/assets/html/graph.html
 jupyter nbconvert mcts.ipynb --to markdown --output ~/Documents/jakee417.github.io/_includes/markdown/mcts.md
